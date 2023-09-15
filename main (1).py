9bfd929d319c6d2da6e741381fe6940c3d00da1a
h8a9r0i <143982703+h8a9r0i@users.noreply.github.com>
2023-09-15 14:59:26 +0530

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,17 @@
+year = 2023
+
+
+
+# To get year (integer input) from the user
+
+# year = int(input("Enter a year: "))
+
+
+
+# divided by 100 means century year (ending with 00)
+
+# century year divided by 400 is leap year
+
+if (year % 400 == 0) and (year % 100 == 0):
+
+    print("{0} is a leap
